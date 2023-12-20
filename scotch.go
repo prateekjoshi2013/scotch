06f43a240846620d710cee8576b82e506a3cc6c8
prateek <prateekjoshi2013@gmail.com>
2023-12-19 21:46:11 -0700

    Added scotch dir structure creation logic

---
+++ scotch.go
@@ -0,0 +1,37 @@
+package scotch
+
+const VERSION = "1.0.0"
+
+func (s *Scotch) New(rootPath string) error {
+	pathConfig := initPaths{
+		rootPath: rootPath,
+		folderNames: []string{
+			"handlers",
+			"migrations",
+			"views",
+			"data",
+			"public",
+			"tmp",
+			"logs",
+			"middlewares",
+		},
+	}
+
+	err := s.Init(pathConfig)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Scotch) Init(p initPaths) error {
+	root := p.rootPath
+	for _, path := range p.folderNames {
+		// create folder if it does not exist
+		err := s.CreateDirIfNotExist(root + "/" + path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
