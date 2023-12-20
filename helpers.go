f2ef638ba7292bcf6b3a3dbb0407d95e2fad8f45
prateek <prateekjoshi2013@gmail.com>
2023-12-19 21:44:41 -0700

    Added helper methods

---
+++ helpers.go
@@ -0,0 +1,14 @@
+package scotch
+
+import "os"
+
+func (s *Scotch) CreateDirIfNotExist(path string) error {
+	const mode = 0755
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.Mkdir(path, mode)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
