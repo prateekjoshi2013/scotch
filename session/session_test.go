f66876c2d5aaf28dcf7aed3bd476cef0b6cdb601
prateek <prateekjoshi2013@gmail.com>
2023-12-21 20:23:27 -0700

    Added session tests using reflect package

---
+++ session/session_test.go
@@ -0,0 +1,43 @@
+package session
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+func TestSession_InitSession(t *testing.T) {
+	s := &Session{
+		CookieLifetime: "100",
+		CookiePersist:  "true",
+		CookieName:     "scotch",
+		CookieDomain:   "localhost",
+		SessionType:    "cookie",
+	}
+
+	var sm *scs.SessionManager
+	sessionManager := s.InitSession()
+	var sessKind reflect.Kind
+	var sessType reflect.Type
+	rv := reflect.ValueOf(sessionManager)
+
+	for rv.Kind() == reflect.Ptr || rv.Kind() == reflect.Interface {
+		fmt.Println("For loop", rv.Kind(), rv.Type(), rv)
+		sessKind = rv.Kind()
+		sessType = rv.Type()
+		rv = rv.Elem()
+	}
+	if !rv.IsValid() {
+		t.Error("invalid type or kind; kind:", rv.Kind(), "type:", rv.Type())
+	}
+
+	if sessKind != reflect.ValueOf(sm).Kind() {
+		t.Error("wrong kind returned testing cookie session. Expected:", reflect.ValueOf(sm).Kind(), "got:", sessKind)
+	}
+
+	if sessType != reflect.ValueOf(sm).Type() {
+		t.Error("wrong type returned testing cookie session. Expected:", reflect.ValueOf(sm).Type(), "got:", sessType)
+	}
+}
