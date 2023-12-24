9a7ff527c6bd3ca752cf124acb13a9de3a6b26e0
prateek <prateekjoshi2013@gmail.com>
2023-12-23 20:00:54 -0700

    Added helper method to generate random string

--- helpers.go
+++ helpers.go
@@ -1,6 +1,23 @@
 package scotch
 
-import "os"
+import (
+	"crypto/rand"
+	"os"
+)
+
+const (
+	randomString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+)
+
+func (s *Scotch) RandomString(n int) string {
+	c, r := make([]rune, n), []rune(randomString)
+	for i := range c {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		c[i] = r[x%y]
+	}
+	return string(c)
+}
 
 func (s *Scotch) CreateDirIfNotExist(path string) error {
 	const mode = 0755
